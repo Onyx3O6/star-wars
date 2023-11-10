c62aa41e71f86e092eb2d43c4f66febe243e1776
Jessy Tibou <tibou.jessy@outlook.fr>
2023-11-10 15:21:03 +0100

    implement song

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-document.getElementById('musique').play();
+window.addEventListener("DOMContentLoaded", (event) => {
+  let mus = document.getElementById("myAudio")
+  function playMus() {
+    mus.play()
+  }
+  setInterval(playMus, 5000)
+})
